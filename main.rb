cb80d0b1a9527667d3346811bf930e9a410f5c8a
She-Codes <n.y.shaw@gmail.com>
2014-03-21 22:04:15 -0400

    added styling and dealer logic

--- main.rb
+++ main.rb
@@ -53,37 +53,50 @@ end
 
 post '/get_name' do
   if params[:player_name].empty?
-    @error = "You forgot to enter a name."
+    @problem = true
     halt erb :get_name
   end
   session[:player_name] = params[:player_name]
+  session[:player_score] = 0
+  session[:dealer_score] = 0
   redirect '/game'
 end
 
 get '/game' do
   @suits = ['hearts', 'clubs', 'diamonds', 'spades']
   @cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king', 'ace']
+  @turn = 'initial'
   session[:deck] = @suits.product(@cards).shuffle!
   session[:player_hand] = []
   session[:dealer_hand] = []
 
   initial_deal(session[:player_hand], session[:deck])
   initial_deal(session[:dealer_hand], session[:deck])
-  @initial_deal = true
+
+  player_total = get_total(session[:player_hand])
+  
+  if player_total == 21
+    @show_buttons = false
+    @message = "You've got Blackjack!"
+    session[:player_score] += 1
+  end
+  
   erb :game
 end
 
 post '/game/player/hit' do
   deal_card(session[:player_hand], session[:deck])
   player_total = get_total(session[:player_hand])
+  @turn = 'hit'
   
   if player_total == 21
     @show_buttons = false
     @message = "You've got Blackjack!"
+    session[:player_score] += 1
   elsif player_total > 21
     @show_buttons = false
     @message = "Sorry, looks like you busted."
-  
+    session[:dealer_score] += 1
   end
   
   erb :game
@@ -92,13 +105,39 @@ end
 post '/game/player/stay' do
   @show_buttons = false
   @message = "You've decided to stay"
+  @turn = 'stay'
   # dealer turn
-  # if get_total(session[:dealer_hand]) < 17
-  #   deal_card(session[:dealer_hand], session[:deck])
-  # end
+  if get_total(session[:dealer_hand]) < 17
+    redirect '/game/dealer/hit'
+  end
+  
   erb :game
 end
 
+get '/game/dealer/hit' do
+  dealer_total = get_total(session[:dealer_hand])
+  @turn = 'stay'
+  while dealer_total < 17
+    deal_card(session[:dealer_hand], session[:deck])
+  end
+
+  if dealer_total == 21
+    @message = "Sorry #{session[:player_name]}, but the dealer hit Blackjack. Better luck next time!"
+    session[:dealer_score] += 1
+  elsif dealer_total > 21
+    @message = "#{session[:player_name]}, you won! The dealer busted with #{dealer_total}!"
+    session[:player_score] += 1
+  else
+    #compare scores
+    redirect '/game/dealer/stay'
+  end
+  erb :game
+end
+
+get '/game/dealer/stay' do
+  dealer_total = get_total(session[:dealer_hand])
+  player_total = get_total(session[:player_hand])
+end
 
 
 
